ca5d528262103fe663e18c9017d2b20a0b3217a1
Lucas A. Machado <des.lucasmachado@gmail.com>
2023-12-29 09:34:55 -0300

    subindo arquivos do desafio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+
+Nokia n = new Nokia("998312582", "3120", "136548745654", 32);
+Iphone i = new Iphone("992427161", "11", "545465481488", 128);
+
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("App Teste");
+
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("App Teste");
